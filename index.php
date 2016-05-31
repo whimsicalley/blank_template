d5c9d66add586de7edd8b4d8a19d268788345f6c
Alley Manalio <amanalio@ufl.edu>
2016-05-31 19:52:16 -0400

    Added content to index and basic css

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Alley Manalio's Blank Template</title>
+</head>
+<body>
+  <h1>Blank WordPress Template</h1>
+    <p>This is my WordPress template.</p>
+</body>
+</html>
