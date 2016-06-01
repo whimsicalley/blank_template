20f2919d537ed39acad426f57b0c109cd79e32f8
Alley Manalio <amanalio@ufl.edu>
2016-05-31 20:01:33 -0400

    added skeleton stylesheets, linked index to css

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html>
 <head>
   <title>Alley Manalio's Blank Template</title>
+  <!-- Link to a CSS file -->
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+  
 </head>
 <body>
-  <h1>Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+  <h1>Alley's Blank WordPress Template</h1>
+  <p>This is my WordPress template.</p>
 </body>
 </html>
