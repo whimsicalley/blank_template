427278d8625c2f6c3f920bc5b3f245353c4fd512
Alley Manalio <amanalio@ufl.edu>
2016-05-31 23:16:52 -0400

    Added basic styling

--- page.php
+++ page.php
@@ -7,7 +7,6 @@
       <?php
         if ( have_posts() ) {
           while ( have_posts() ) {
-            /* OUR DATA CONTEXT IS DEFINED */
             the_post(); ?>
 
             <h2><?php the_title(); ?></h2>
