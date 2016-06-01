2598ddb573bedb8a5846e3e2d57a0f866acb932c
Alley Manalio <amanalio@ufl.edu>
2016-05-31 20:15:02 -0400

    Added grid-based system to index

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 <html>
 <head>
   <title>Alley Manalio's Blank Template</title>
-  <!-- Link to a CSS file -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-  
 </head>
 <body>
-  <h1>Alley's Blank WordPress Template</h1>
-  <p>This is my WordPress template.</p>
+  <div class="container">
+
+    <header class="row">
+      <div class="twelve columns">
+        <h1>Alley's Blank WordPress Template</h1>
+        <p>This is my WordPress template.</p>
+      </div>
+    </header>
+
+    <section class="row">
+      <div class="twelve columns">
+        <h2>Section content - cool</h2>
+        <p>This is some section content.</p>
+      </div>
+    </section>
+
+    <footer class="row">
+      <div class="twelve columns">
+        <h2>This is the footer</h2>
+        <p>This is some footer content.</p>
+      </div>
+    </footer>
+
+  </div>
 </body>
 </html>
